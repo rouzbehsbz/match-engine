8a25a0d7428282ddfb25ffac9630be1885623f9b
rouzbehsbz <rouzbehsbz@gmail.com>
2024-05-15 12:36:29 +0330

    feat: add some project todos and clean test in main

---
+++ docs/todos.md
@@ -0,0 +1,12 @@
+# Business Logic
+
+- Asset precisions in calculations
+- Trade fees calculations
+- Add time in force option for orders
+
+# Software
+
+- Event sourcing for orders, trades and balances lifecycle
+- Add unit and integrate tests
+- Check single thread performance vs multi threaded engine and also single worker vs multi worker async runtime
+- If engine runs in single thread. conside using `Rc` over `Arc`
--- src/main.rs
+++ src/main.rs
@@ -13,45 +13,6 @@ pub mod container;
 pub mod engine;
 pub mod presentation;
 
-//TODO: handle asset precisions
-//TODO: handle trade fees
-//TODO: handle event sourcing
-//TODO: handle TIF for orderbook
+fn main() {
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config = TomlConfigManager::from_file("./config.test.toml");
-    let container = Container::new(&config);
-
-    let Rouzbeh: UserId = 1;
-    let Kasra: UserId = 2;
-
-    let BTC = 1;
-    let USDT = 2;
-
-    container.balance_service.change_balance(Rouzbeh, USDT, BusinessType::Deposit, 1, BalanceType::Available, Decimal::from(1000))?;
-    container.balance_service.change_balance(Kasra, BTC, BusinessType::Deposit, 1, BalanceType::Available, Decimal::from(1000))?;
-
-    container.engine_service.place_order(1, Rouzbeh, Some(Decimal::from(20)), Decimal::from(10), OrderSide::Bid)?;
-    container.engine_service.place_order(1, Rouzbeh, Some(Decimal::from(17)), Decimal::from(5), OrderSide::Bid)?;
-    container.engine_service.place_order(1, Kasra, Some(Decimal::from(18)), Decimal::from(50), OrderSide::Ask)?;
-
-    let ob = container.engine_service.get_market_orderbook(1);
-
-    println!("{:?}", ob);
-
-    println!("Rouzbeh BTC {:?}", container.balance_service.get_balance_status(Rouzbeh, BTC));
-    println!("Rouzbeh USDT {:?}", container.balance_service.get_balance_status(Rouzbeh, USDT));
-    println!("----");
-    println!("Kasra BTC {:?}", container.balance_service.get_balance_status(Kasra, BTC));
-    println!("Kasra USDT {:?}", container.balance_service.get_balance_status(Kasra, USDT));
-
-    // Server::builder()
-    // .add_service(TradeServer::new(TradeController::new(
-    //     container.engine_service,
-    //     container.balance_service,
-    // )))
-    // .serve("0.0.0.0:3000".parse()?)
-    // .await?;
-
-    Ok(())
 }
